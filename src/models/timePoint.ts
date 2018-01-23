a16e26661f58f2399ed20cdca29551b0d7d5b1a2
Joakim Hägglund <zorlius@gmail.com>
2018-01-23 09:23:02 +0100

    Styling and run command

--- src/models/timePoint.ts
+++ src/models/timePoint.ts
@@ -4,7 +4,7 @@ export class TimePoint {
     date: moment.Moment;
     name: string
     constructor(name: string, date: string){
-        this.date = moment(date);
+        this.date = moment(date).add(1, 'day');
         this.name = name;
     }
 }
