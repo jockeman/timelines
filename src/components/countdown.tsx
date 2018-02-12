f0917b49b1a3bf7ac8dd5ffba1a3b7bfe4aea4f2
Joakim Hägglund <joakim.hagglund@caspeco.se>
2018-02-12 08:45:37 +0100

    Added day/ms-mode to countdown

--- src/components/countdown.tsx
+++ src/components/countdown.tsx
@@ -2,10 +2,35 @@ import * as React from 'react';
 import moment from 'moment';
 import { TimePoint } from '../models/timePoint';
 
+enum CountdownMode {
+    Day,
+    Ms
+}
+
 interface ICountdownProps {
     to: TimePoint;
     current: moment.Moment;
+    mode?: CountdownMode;
 }
 export const Countdown: React.SFC<ICountdownProps> = (props) => {
-    return <>{props.to.name} om {props.to.date.clone().endOf('day').diff(props.current, 'days').toString()} dagar</>;
+    let mode = props.mode || CountdownMode.Day;
+    let countdown = ""
+    switch (mode) {
+        case CountdownMode.Day:
+            const days = props.to.date.clone().endOf('day').diff(props.current, 'day');
+            if(days <= 1){
+                const hours = props.to.date.clone().diff(props.current, 'hour');
+                countdown = `${hours}:${props.to.date.clone().diff(props.current, 'minutes') - hours * 60}`
+            } else {
+                countdown = days + ' dagar';
+            }
+            break;
+        case CountdownMode.Ms:
+            countdown = props.to.date.clone().diff(props.current, 'ms').toString() + ' ms'
+          break;
+    
+        default:
+            break;
+    }
+    return <>{props.to.name} om {countdown}</>;
 };
